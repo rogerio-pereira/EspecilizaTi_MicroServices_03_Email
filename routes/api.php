ae2dc3dd0044345059f10093d75c1a56ed823ad3
Rogerio Pereira <hi@rogeriopereira.dev>
2021-08-04 20:49:19 -0400

    Aula: 02 - Enviar E-mail de Boas Vindas Micro Laravel

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Jobs\CompanyCreatedJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
+    return response()->json(['message' => 'success']);
+});
+
+
+Route::get('/test', function() {
+    CompanyCreatedJob::dispatch('teste2@email.com');
+
     return response()->json(['message' => 'success']);
 });
